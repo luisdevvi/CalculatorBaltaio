cbf13eed8cc8b9275405640105121bd6df39f001
luisdevvi <luis.devvi@gmail.com>
2024-10-29 23:07:29 -0300

    Add: subtraction function

--- CalculatorBaltaio/Program.cs
+++ CalculatorBaltaio/Program.cs
@@ -21,5 +21,20 @@
 
             Console.WriteLine($"\nThe result of sum is {result}.");
         }
+
+        static void Subtratcion()
+        {
+            Console.Clear();
+
+            Console.WriteLine("First value: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSecond value: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float result = v1 - v2;
+
+            Console.WriteLine($"\nThe result of subtraction is {result}.");
+        }
     }
 }
