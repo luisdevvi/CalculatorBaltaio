0537f520027627a2885cb780302cc48a7dcb7f07
luisdevvi <luis.devvi@gmail.com>
2024-10-29 23:19:39 -0300

    Add: division function

--- CalculatorBaltaio/Program.cs
+++ CalculatorBaltaio/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Sum();
+            //Sum();
+            //Subtraction();
+            Division();
         }
 
         static void Sum()
@@ -35,6 +37,21 @@
             float result = v1 - v2;
 
             Console.WriteLine($"\nThe result of subtraction is {result}.");
+        }
+
+        static void Division()
+        {
+            Console.Clear();
+
+            Console.WriteLine("First value: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSecond value: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float result = v1 / v2;
+
+            Console.WriteLine($"\nThe result of division is {result}.");
         }
     }
 }
